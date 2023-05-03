ee2f0a60db538d7b77cc56c091f2c3456f15b8e5
Joaco182004 <87869138+Joaco182004@users.noreply.github.com>
2023-05-03 15:42:34 -0500

    Delete finanzas directory

--- finanzas/src/main/java/com/example/finanzas/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.finanzas.repositories;
-
-import com.example.finanzas.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-    Customer findByEmailAndPassword(String email, String password);
-    Customer findByEmail(String email);
-}
--- finanzas/src/main/java/com/example/finanzas/repositories/FinancialRunRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.finanzas.repositories;
-
-import com.example.finanzas.entities.Customer;
-import com.example.finanzas.entities.FinancialRun;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FinancialRunRepository extends JpaRepository<FinancialRun,Long> {
-
-}
